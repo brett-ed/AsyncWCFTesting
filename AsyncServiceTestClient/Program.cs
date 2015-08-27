328623deff37f87d18c049a92dd4101f7c4c326e
Brett Edwards <brettandjj2@gmail.com>
2015-08-27 07:06:36 -0400

    Test async service code

---
+++ AsyncServiceTestClient/Program.cs
@@ -0,0 +1,46 @@
+﻿using AsyncServiceTestClient.WcfTestService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncServiceTestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string>(new string[] { "Brett", "JJ", "Brandy", "Zach" });
+            //Console.WriteLine("waiting 10 seconds for all assemblies to load");
+            //Thread.Sleep(10000);
+            Console.WriteLine("Dispatching asynchronous service calls.....");
+            DisplayText(list)
+                .ContinueWith(t => Console.WriteLine("All calls dispatched, awaiting results....."));
+
+            Console.ReadKey();
+        }
+
+        private static async Task DisplayText(List<string> list)
+        {
+            list.ForEach(async item =>
+           {
+               try
+               {
+                   Task<string> task = new DemoServiceClient().GetStringDataAsync(item);
+                   if ( task == await Task.WhenAny(task, Task.Delay(9000)))
+                   {
+                       Console.WriteLine(await task);
+                   }
+                   else
+                       Console.WriteLine("The execution has timed out.");
+               }
+               catch (Exception e)
+               {
+                   Console.WriteLine(e.Message);
+               }
+           });
+        }
+    }
+}
---
+++ AsyncWCFTesting/DemoService.svc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncWCFTesting
+{
+    public class DemoService : IDemoService
+    {
+        public async Task<string> GetStringDataAsync(string name)
+        {
+            return await Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(new Random().Next(1000, 6000));
+                return $"The passed name is {name}";
+            });
+        }
+    }
+}
